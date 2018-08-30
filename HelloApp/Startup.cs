14e29ed39759fc6fbdb382e59d79e05413111dae
Вадим Зверев <WadimZverev@gmail.com>
2018-08-31 02:48:39 +0400

    Использование метода UseDirectoryBrowser() для возможности просмотра каталогов приложения.

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace HelloApp
 {
@@ -15,6 +17,7 @@ namespace HelloApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,11 +28,11 @@ namespace HelloApp
                 app.UseDeveloperExceptionPage();
             }
 
-            DefaultFilesOptions options = new DefaultFilesOptions();
-            options.DefaultFileNames.Clear(); // удаляем имена файлов по умолчанию
-            options.DefaultFileNames.Add("Hello.html"); // добавляем новое имя файла
-            app.UseDefaultFiles(options); // установка параметров
-
+            app.UseDirectoryBrowser( new DirectoryBrowserOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+                RequestPath = new PathString("/pages")
+            });
             app.UseStaticFiles();
 
             app.Run(async (context) =>
