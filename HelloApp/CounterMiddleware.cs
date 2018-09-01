6ecd84fed3358fe5ae03c5d187d7b4e41ba6cbfb
Вадим Зверев <WadimZverev@gmail.com>
2018-09-02 02:29:21 +0400

    Приготовления к  демонстрации жизненых циклов при разном подходе

---
+++ HelloApp/CounterMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using HelloApp.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    public class CounterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private int i = 0; // счетчик запросов
+
+        public CounterMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ICounter counter,
+            CounterService counterService)
+        {
+            i++;
+            context.Response.ContentType = "text/html;charset=utf-8";
+            await context.Response.WriteAsync($"Запрос {i}; Counter: {counter.Value};" +
+                    $" Service: {counterService.Counter.Value}");
+        }
+    }
+}
--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -17,6 +17,18 @@ namespace HelloApp
         {
         }
 
+
+
+        // Transient - объект сервиса создается каждый раз, когда к нему обращается запрос.
+        //Подобная модель жизненного цикла наиболее подходит для легковесных сервисов, 
+        //которые не хранят данных о состоянии.
+
+        // Scoped - для каждого запроса создается свой объект сервиса.
+
+        // Singleton - объект сервиса создается при первом обращении к нему,
+        // все последующие запросы используют один и тот же ранее созданный объект сервиса.
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
