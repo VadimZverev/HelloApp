077f1c5951b74347a005b79b7f08e5eb6e08b44c
Вадим Зверев <WadimZverev@gmail.com>
2018-09-03 01:17:42 +0400

    Возникновение ошибки из-за того, что неправильно определен сервис для singleton компонента(он может принемать только себе подобные сервисы)

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -15,6 +15,8 @@ namespace HelloApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ITimer, Timer>();
+            services.AddScoped<TimeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,8 @@ namespace HelloApp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMiddleware<TimerMiddleware>();
 
             app.Run(async (context) =>
             {
