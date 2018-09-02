625584c122c420a10acd8770fca5e1104b97bd58
Вадим Зверев <WadimZverev@gmail.com>
2018-09-03 01:06:09 +0400

    Создание компонента

---
+++ HelloApp/TimerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    public class TimerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        TimeService _timeService;
+
+        public TimerMiddleware(RequestDelegate next, TimeService timeService)
+        {
+            _next = next;
+            _timeService = timeService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.ContentType = "text/html; charset=utf-8";
+            await context.Response.WriteAsync($"текущее время: {_timeService.GetTime()}");
+        }
+    }
+}
