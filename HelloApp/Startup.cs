090447e6f22ae6b20567b8b92e346f57c1195b0d
Вадим Зверев <WadimZverev@gmail.com>
2018-09-03 22:09:25 +0400

    Создаём конфигурацию на базовой основе

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -5,26 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HelloApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup()
+        {
+            // строитель конфигурации
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    {"firsname", "Tom" },
+                    {"age", "31" }
+                });
+
+            // создаём конфигурацию
+            AppConfiguration = builder.Build();
+        }
+
+        // свойство, которое будет хранить конфигурацию
+        public IConfiguration AppConfiguration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
