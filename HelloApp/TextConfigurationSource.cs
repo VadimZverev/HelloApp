1229433652f3ee460bbce600cbca274cab0a2569
Вадим Зверев <WadimZverev@gmail.com>
2018-09-04 22:40:48 +0400

    Определяем класс источника иноформации для обращения к провайдеру.

---
+++ HelloApp/TextConfigurationSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HelloApp
+{
+    public class TextConfigurationSource : IConfigurationSource
+    {
+        public string FilePath { get; set; }
+
+        public TextConfigurationSource(string filename)
+        {
+            FilePath = filename;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            // получаем полный путь для файла
+            string filePath = builder.GetFileProvider().GetFileInfo(FilePath).PhysicalPath;
+
+            return new TextConfigurationProvider(filePath);
+        }
+    }
+}
