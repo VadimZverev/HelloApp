67a3825dd927d2b66a8f639504c2e9145e17e666
Вадим Зверев <WadimZverev@gmail.com>
2018-09-05 23:26:48 +0400

    Настройка запуска конфигурации через IOptions

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -5,16 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HelloApp
 {
     public class Startup
     {
+        public IConfiguration AppConfiguration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            AppConfiguration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Настройка параметров DI
+            services.AddOptions();
+
+            // Создание объекта Person по ключам из конфигурации
+            services.Configure<Person>(AppConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +38,7 @@ namespace HelloApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMiddleware<PersonMiddleware>();
         }
     }
 }
