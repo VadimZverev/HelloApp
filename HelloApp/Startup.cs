d07add2a7f02a8595f937958437a526cb574fd46
Вадим Зверев <WadimZverev@gmail.com>
2018-09-05 23:52:27 +0400

    Подключение сервиса маршрутизации и добавление созданного маршрута в конвеер обработки запросов

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HelloApp
@@ -15,6 +16,7 @@ namespace HelloApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,28 @@ namespace HelloApp
                 app.UseDeveloperExceptionPage();
             }
 
+            // определяем обработчик маршрута
+            var myRouteHandler = new RouteHandler(Handle);
+
+            // создаём маршрут, используя обработчик
+            var routeBuilder = new RouteBuilder(app, myRouteHandler);
+
+            // само определение маршрута - он должен соответствовать запросу {controller}/{action}
+            routeBuilder.MapRoute("default", "{controller}/{action}");
+
+            // строим маршрут
+            app.UseRouter(routeBuilder.Build());
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+        }
+
+        // обработчик маршрута
+        private async Task Handle(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello ASP.NET Core!");
         }
     }
 }
