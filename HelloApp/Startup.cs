93ef0f82960acf579ed5014c28cc92e4cb30bf24
Вадим Зверев <WadimZverev@gmail.com>
2018-10-07 00:31:13 +0400

    Установка пользовательского ограничения

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HelloApp
@@ -15,6 +16,7 @@ namespace HelloApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,14 @@ namespace HelloApp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var routeBuilder = new RouteBuilder(app);
+
+            // Установка пользовательского ограничения
+            routeBuilder.MapRoute("default",
+                "{controller}/{action}/{id?}",
+                null,
+                new { myConstraint = new CustomConstraint("/Home/Index/12") });
 
             app.Run(async (context) =>
             {
