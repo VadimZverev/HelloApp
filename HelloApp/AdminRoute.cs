56ca34cc680abcd112caeca084134c80a89d6899
Вадим Зверев <WadimZverev@gmail.com>
2018-10-07 02:00:26 +0400

    Коментарии к коду

--- HelloApp/AdminRoute.cs
+++ HelloApp/AdminRoute.cs
@@ -18,11 +18,17 @@ namespace HelloApp
 
             if (url.StartsWith("/Admin", StringComparison.OrdinalIgnoreCase))
             {
+                // Обрабатывается только тем делегатом, который установлен для свойства 
+                // Handler
                 context.Handler = async ctx =>
                 {
                     ctx.Response.ContentType = "text/html;charset=utf=8";
                     await ctx.Response.WriteAsync("Привет Admin!");
                 };
+
+                // другой вариант, который отличается от выше описанного, тем
+                // что будет дальше идти по списку маршрутов.
+                // await context.HttpContext.Response.WriteAsync("Привет Admin!");
             }
 
             return Task.CompletedTask;
