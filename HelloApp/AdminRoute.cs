78c22432dbf86ecd61180202d874ea274c587fdf
Вадим Зверев <WadimZverev@gmail.com>
2018-10-07 01:43:05 +0400

    Добавлене пользовательского класса для создания своего маршрута

---
+++ HelloApp/AdminRoute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    public class AdminRoute : IRouter
+    {
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            string url = context.HttpContext.Request.Path.Value.TrimEnd('/');
+
+            if (url.StartsWith("/Admin", StringComparison.OrdinalIgnoreCase))
+            {
+                context.Handler = async ctx =>
+                {
+                    ctx.Response.ContentType = "text/html;charset=utf=8";
+                    await ctx.Response.WriteAsync("Привет Admin!");
+                };
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
