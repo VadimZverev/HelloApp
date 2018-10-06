a72552138416bffaee276fcf61a75d4d9b36aecd
Вадим Зверев <WadimZverev@gmail.com>
2018-10-06 20:11:20 +0400

    Работа с маршрутами с помощью методов

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HelloApp
@@ -15,6 +16,7 @@ namespace HelloApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,28 @@ namespace HelloApp
                 app.UseDeveloperExceptionPage();
             }
 
+            var myRouteHandler = new RouteHandler(Handle);
+            var routeBuilder = new RouteBuilder(app, myRouteHandler);
+            routeBuilder.MapRoute("default", "{action=Index}/{name}-{year}");
+            app.UseRouter(routeBuilder.Build());
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+        }
+
+        private async Task Handle(HttpContext context)
+        {
+            // Позволяет получить объект RouteData и извлечь значения из него
+            var routeValues = context.GetRouteData().Values;
+
+            // По индексу извлекает строку каждого значения значения отдельно по индексу
+            var action = routeValues["action"].ToString();
+            var name = routeValues["name"].ToString();
+            var year = routeValues["year"].ToString();
+
+            await context.Response.WriteAsync($"action: {action} | name: {name} | year: {year}");
         }
     }
 }
