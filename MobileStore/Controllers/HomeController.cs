7ed34bb479bf05439a8bbb30bd7e7ce98cbb3444
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-10-12 14:52:27 +0400

    Добавление методов покупки

--- MobileStore/Controllers/HomeController.cs
+++ MobileStore/Controllers/HomeController.cs
@@ -21,5 +21,22 @@ namespace MobileStore.Controllers
         {
             return View(context.Phones.ToList());
         }
+
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            context.Add(order);
+            // сохраняем в бд все изменени
+            context.SaveChanges();
+
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
     }
 }
