b883cd7f703f08bb657fddd7e9735bbe70d8d5e1
Вадим Зверев <WadimZverev@gmail.com>
2018-10-15 00:13:56 +0400

    JsonResult

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloApp.Controllers
 {
@@ -21,6 +22,20 @@ namespace HelloApp.Controllers
         {
             double square = altitude * height / 2;
             return Content($"Площадь треугольника с основанием {altitude} и высотой {height} равна {square}");
+        }
+
+        // Использование JsonResult
+        public JsonResult GetName()
+        {
+            string name = "Tom";
+            return Json(name);
+        }
+
+        // Использование JsonResult для передачи объекта
+        public JsonResult GetUser()
+        {
+            User user = new User { Name = "Tom", Age = 28 };
+            return Json(user);
         }
     }
 }
