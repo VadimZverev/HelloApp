6839f15084f1594c413c5d348dfe0a99cffced96
Вадим Зверев <WadimZverev@gmail.com>
2018-10-14 19:28:15 +0400

    Передача сложных объектов

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloApp.Controllers
 {
@@ -22,6 +23,12 @@ namespace HelloApp.Controllers
         {
             double s = a * h / 2;
             return $"Площадь треугольника с основанием {a} и высотой {h} равна {s}";
+        }
+
+        // Передача сложных объектов
+        public string SquareObject(Geometry geometry)
+        {
+            return $"Площадь треугольника с основанием {geometry.Altitude} и высотой {geometry.Height} равна {geometry.GetSquare()}";
         }
     }
 }
---
+++ HelloApp/Models/Geometry.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloApp.Models
+{
+    public class Geometry
+    {
+        public int Altitude { get; set; } // Основание
+        public int Height { get; set; } // высота
+
+        public double GetSquare() // вычисление площади треугольника
+        {
+            return Altitude * Height / 2;
+        }
+    }
+}
