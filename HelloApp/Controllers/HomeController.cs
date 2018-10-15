48c7a455d8b676afce6570069d1498de1b26578f
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-10-15 11:43:29 +0400

    Загрузка через физический путь PhysicalFileResult

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace HelloApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _appEnvironment;
+        public HomeController(IHostingEnvironment appEnvironment)
+        {
+            appEnvironment = _appEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Path.Combine(_appEnvironment.ContentRootPath, "Files/book.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательный параметр
+            string file_name = "book.pdf";
+            return PhysicalFile(file_path, file_type, file_name);
         }
     }
 }
