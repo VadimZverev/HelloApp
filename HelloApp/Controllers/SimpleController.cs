47633563764b2df7f45df3f52a44db9caf275deb
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-10-16 12:40:23 +0400

    получение сервиса через запрос сервиса. RequestServices

--- HelloApp/Controllers/SimpleController.cs
+++ HelloApp/Controllers/SimpleController.cs
@@ -1,13 +1,22 @@
 ﻿using HelloApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection; // библиотека для получения сервиса из запроса
 
 namespace HelloApp.Controllers
 {
     public class SimpleController : Controller
     {
+        // Передача зависимости через параметры метода
         public string Index([FromServices] ITimeService timeService)
         {
             return timeService.Time;
+        }
+
+        // Получение доступа к сервису через запроса сервиса
+        public string Answer()
+        {
+            ITimeService timeService = HttpContext.RequestServices.GetService<ITimeService>(); // получение доступа к сервису
+            return timeService?.Time;
         }
     }
 }
