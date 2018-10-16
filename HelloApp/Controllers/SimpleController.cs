3f3ed5b36cae1f8d6cbd00e0cc45fce4e00e2273
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-10-16 12:28:56 +0400

    Передача зависемостей в методы. FromService

---
+++ HelloApp/Controllers/SimpleController.cs
@@ -0,0 +1,13 @@
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Controllers
+{
+    public class SimpleController : Controller
+    {
+        public string Index([FromServices] ITimeService timeService)
+        {
+            return timeService.Time;
+        }
+    }
+}
