5ba8b86098cd119cbc2ddf7eda5575e393db6ae1
Вадим Зверев <WadimZverev@gmail.com>
2018-11-07 19:34:15 +0400

    Указание атрибута "Route"

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ namespace HelloApp.Controllers
 {
     public class HomeController : Controller
     {
+        // С помощью атрибута "Route" указываятся шаблон маршрута,
+        // с помощью которого будет обращение к методу действия.
+        [Route("homepage")]
         public IActionResult Index()
         {
             return View();
