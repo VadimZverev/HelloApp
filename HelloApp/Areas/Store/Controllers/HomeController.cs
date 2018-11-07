abe280f77e42dfa7e1c19d757d6fe74b07c8f7e9
Вадим Зверев <WadimZverev@gmail.com>
2018-11-07 21:17:44 +0400

    Добавление контроллера и представления в область, установка атрибута для сопоставления контроллера с  областью.

---
+++ HelloApp/Areas/Store/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Areas.Store.Controllers
+{
+    [Area("Store")] //Необходимо для того, чтобы контроллер принадлежал к нужной области
+    public class HomeController : Controller
+    {   
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
