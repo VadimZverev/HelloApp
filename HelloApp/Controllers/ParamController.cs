06e68ba336b3fdffd8b0772258cb7459c6af85cb
Вадим Зверев <WadimZverev@gmail.com>
2018-11-07 20:28:59 +0400

    Параметры controller и action

---
+++ HelloApp/Controllers/ParamController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Controllers
+{
+    public class ParamController : Controller
+    {
+        // При использовании action и controller исп. "[ ]"
+        // Также есть возможно использования их по отдельности:
+        // [Route("[controller]")]
+        // [Route("[action]")]
+        // [Route("[controller]/[action]/{id?}")]
+        [Route("[action]/[controller]/{id?}")]
+        public IActionResult Index(string id)
+        {
+            string controller = RouteData.Values["controller"].ToString();
+            string action = RouteData.Values["action"].ToString();
+
+            return Content($"controller: {controller} | action: {action}");
+        }
+    }
+}
