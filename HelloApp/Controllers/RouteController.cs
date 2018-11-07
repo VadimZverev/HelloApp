3bfc3cbb52aa8195dd40d0333b910d7c766d3f27
Вадим Зверев <WadimZverev@gmail.com>
2018-11-07 20:58:37 +0400

    Множественные маршруты

---
+++ HelloApp/Controllers/RouteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Controllers
+{
+    [Route("[Controller]")]
+    public class RouteController : Controller
+    {
+        [Route("")]         // сопоставляется с Route
+        [Route("Action")]   // сопоставляется с Route/Action
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
---
+++ HelloApp/Controllers/RoutesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Controllers
+{
+    [Route("Store")]
+    [Route("[controller]")]
+    public class RoutesController : Controller
+    {
+        [Route("Main")]     // сопоставляется с Routes/Main, либо с Store/Main
+        [Route("Index")]    // сопоставляется с Routes/Index, либо с Store/Index
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
