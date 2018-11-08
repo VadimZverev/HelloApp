88f885ce457dacec11e9c38904b2d8e75b2df485
Вадим Зверев <WadimZverev@gmail.com>
2018-11-08 23:51:44 +0400

    Создание компонента представления, посредством наследования от ViewComponent

---
+++ HelloApp/Components/BestPhoneV2.cs
@@ -0,0 +1,30 @@
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloApp.Components
+{
+    // Используется способ наследования от ViewComponent
+    public class BestPhoneV2 : ViewComponent
+    {
+        List<Phone> phones;
+        public BestPhoneV2()
+        {
+            phones = new List<Phone>
+            {
+                new Phone {Title="iPhone 7", Price=56000},
+                new Phone {Title="Idol S4", Price=26000},
+                new Phone {Title="Elite x3", Price=55000},
+                new Phone {Title="Honor 8", Price=23000}
+            };
+        }
+
+        public string Invoke()
+        {
+            var item = phones.OrderByDescending(x => x.Price).Take(1).FirstOrDefault();
+
+            return $"Самый дорогой телефон: {item.Title}  Цена: {item.Price}";
+        }
+    }
+}
