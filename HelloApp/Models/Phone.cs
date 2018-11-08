d4b214a6c59b4d3f80c3dccc9ba5a766e477b91a
Вадим Зверев <WadimZverev@gmail.com>
2018-11-09 00:04:14 +0400

    Подготовка данных для примера

---
+++ HelloApp/Models/Phone.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloApp.Models
+{
+    public class Phone
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,9 @@ namespace HelloApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Регистрация зависимости.
+            services.AddTransient<IRepository, PhoneRepository>();
+
             // Добавление MVC, как сервис в проект
             services.AddMvc();
         }
