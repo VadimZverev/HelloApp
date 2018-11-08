f9d66fc07b39917b52aee9fb7ac645d127bfce53
Вадим Зверев <WadimZverev@gmail.com>
2018-11-08 23:53:57 +0400

    Применение атрибута ViewComponent

---
+++ HelloApp/Components/BestPhoneV3.cs
@@ -0,0 +1,31 @@
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloApp.Components
+{
+    // Используется способ применения атрибута
+    [ViewComponent]
+    public class BestPhoneV3
+    {
+        List<Phone> phones;
+        public BestPhoneV3()
+        {
+            phones = new List<Phone>
+            {
+                new Phone {Title="iPhone 7", Price=56000},
+                new Phone {Title="Idol S4", Price=26000},
+                new Phone {Title="Elite x3", Price=55000},
+                new Phone {Title="Honor 8", Price=23000}
+            };
+        }
+
+        public string Invoke()
+        {
+            var item = phones.OrderByDescending(x => x.Price).Take(1).FirstOrDefault();
+
+            return $"Самый дорогой телефон: {item.Title}  Цена: {item.Price}";
+        }
+    }
+}
