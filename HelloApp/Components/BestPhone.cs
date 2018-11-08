c4d7adcfcf47627d4a936e599dfb8c69cc507e1a
Вадим Зверев <WadimZverev@gmail.com>
2018-11-09 00:14:43 +0400

    Передача данных во ViewComponent через параметры метода

---
+++ HelloApp/Components/BestPhone.cs
@@ -0,0 +1,21 @@
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HelloApp.Components
+{
+    public class BestPhone : ViewComponent
+    {
+        IRepository repo;
+        public BestPhone(IRepository r)
+        {
+            repo = r;
+        }
+
+        public string Invoke(int maxPrice, int minPrice = 0)
+        {
+            int count = repo.GetPhones().Count(x => x.Price < maxPrice && x.Price > minPrice);
+            return $"В диапозоне от {minPrice} до {maxPrice} найдено {count} модели(ей)";
+        }
+    }
+}
