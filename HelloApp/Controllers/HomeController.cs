3b2e20c3193c73bb7387239ff1cec6442cc6d3a8
Вадим Зверев <WadimZverev@gmail.com>
2018-11-09 23:46:29 +0400

    Инициализация и передача объектов в представление

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace HelloApp.Controllers
 {
     public class HomeController : Controller
     {
+        List<Company> companies;
+        List<Phone> phones;
+
+        // Инициализация списков тестовыми данными
+        public HomeController()
+        {
+            Company apple = new Company { Id = 1, Name = "Apple", Country = "США" };
+            Company microsoft = new Company { Id = 1, Name = "Microsoft", Country = "США" };
+            Company google = new Company { Id = 1, Name = "Google", Country = "США" };
+            companies = new List<Company> { apple, microsoft, google };
+
+            phones = new List<Phone>
+            {
+                new Phone{ Id = 1, Manufacturer = apple, Name = "iPhone 6S", Price = 56000 },
+                new Phone{ Id = 2, Manufacturer = apple, Name = "iPhone 5S", Price = 41000 },
+                new Phone{ Id = 3, Manufacturer = microsoft, Name = "Lumia 550", Price = 9000 },
+                new Phone{ Id = 4, Manufacturer = microsoft, Name = "Lumia 950", Price = 40000 },
+                new Phone{ Id = 5, Manufacturer = google, Name = "Nexus 5X", Price = 30000 },
+                new Phone{ Id = 6, Manufacturer = google, Name = "Nexus 6P", Price = 50000 }
+            };
+        }
+
+        // передача списка в представление
         public IActionResult Index()
         {
-            return View();
+            return View(phones);
         }
     }
 }
