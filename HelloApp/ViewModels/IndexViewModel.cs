809c8fb48bb03b7039c154d71386c8bc420e7f07
Вадим Зверев <WadimZverev@gmail.com>
2018-11-10 00:03:46 +0400

    Создание модели представления для передачи всех данных

---
+++ HelloApp/ViewModels/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using HelloApp.Models;
+using System.Collections.Generic;
+
+namespace HelloApp.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Phone> Phones { get; set; }
+        public IEnumerable<CompanyModel> Companies { get; set; }
+    }
+}
