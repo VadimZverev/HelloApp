f6f4cb3c5599bbd43e348af8ca05cc47e14355e9
Вадим Зверев <WadimZverev@gmail.com>
2018-11-10 18:34:55 +0400

    Создание провайдера для модели привзки

---
+++ HelloApp/Infrastructure/CustomDateTimeModelBinderProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System;
+
+namespace HelloApp.Infrastructure
+{
+    public class CustomDateTimeModelBinderProvider : IModelBinderProvider
+    {
+        // Содержит пользовательскую модель привязки
+        private readonly IModelBinder binder =
+            new CustomDateTimeModelBinder(new SimpleTypeModelBinder(typeof(DateTime)));
+
+        // Метод, где можно получить тип данных, для которых выполняется привязка.
+        // Если тип совпадает с запрашиваемым ему передаётся необходимая модель.
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            return context.Metadata.ModelType == typeof(DateTime) ? binder : null;
+        }
+    }
+}
