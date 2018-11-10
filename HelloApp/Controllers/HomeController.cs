972eb53e847a5b2f745abbbc5e9c38b0dc01d897
Вадим Зверев <WadimZverev@gmail.com>
2018-11-10 16:48:27 +0400

    Источники привязки FromQuery

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -30,10 +30,23 @@ namespace HelloApp.Controllers
         }
 
         // использование атрибута [FromHeader] для извлечения данных
-        // применяется если есть в методе 1 парметр, иначе вызывается исключение
+        // применяется, если есть в методе 1 парметр, иначе вызывается исключение
         public IActionResult GetUserAgent([FromHeader(Name = "User-Agent")] string userAgent)
         {
             return Content(userAgent);
+        }
+
+        public IActionResult AddUserQuery()
+        {
+            return View();
+        }
+
+        // Привязчик FromQuery игнорирует форму и сразу читает из строки запроса
+        [HttpPost]
+        public IActionResult AddUserQuery([FromQuery] User user)
+        {
+            string userInfo = $"Name: {user.Name} Age: {user.Age}";
+            return Content(userInfo);
         }
     }
 }
