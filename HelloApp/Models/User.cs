42cb0a9763a11c1eee4d043ad58f9bc15ecc439c
Вадим Зверев <WadimZverev@gmail.com>
2018-11-10 16:06:00 +0400

    BindingBehavior атрибут

--- HelloApp/Models/User.cs
+++ HelloApp/Models/User.cs
@@ -6,11 +6,15 @@ namespace HelloApp.Models
     {
         public int Id { get; set; }
 
-        [BindRequired] // атрибут используется для указания обязательного указания свойства
+        //[BindRequired] // атрибут используется для указания обязательного указания свойства
+        [BindingBehavior(BindingBehavior.Required)] // аналогичное действие предыдущему
         public string Name { get; set; }
+
+        [BindingBehavior(BindingBehavior.Optional)] // указывает на опциональность выбора
         public int Age { get; set; }
 
-        [BindNever] // указывает, что свойства никогда не участвует в привязке(даже если указать данные).
+        //[BindNever] // указывает, что свойства никогда не участвует в привязке(даже если указать данные).
+        [BindingBehavior(BindingBehavior.Never)] //аналогичное действие предыдущему
         public bool HasRight { get; set; }
     }
 }
