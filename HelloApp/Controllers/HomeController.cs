066348a6eba79d33fde40bfb0b9b031007a6f079
Вадим Зверев <WadimZverev@gmail.com>
2018-11-10 17:06:30 +0400

    Подготовка данных для примера

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HelloApp.Models;
 
 namespace HelloApp.Controllers
 {
     public class HomeController : Controller
     {
+        static List<Event> events;
+        public HomeController()
+        {
+            if (events == null)
+            {
+                events = new List<Event>();
+            }
+        }
+
         public IActionResult Index()
         {
+            return View(events);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Event ev)
+        {
+            ev.Id = Guid.NewGuid().ToString();
+            events.Add(ev);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ HelloApp/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloApp.Models
+{
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }            // название событие
+        public DateTime EventDate { get; set; }     // дата и время событие
+    }
+}
